8f2f8918f89bb3b70df6ac8bf0b9b322d595b80d
Narjisha K <144308345+NarjishaK@users.noreply.github.com>
2025-05-23 16:07:57 +0530

    build

--- src/Helper/handleapi.js
+++ src/Helper/handleapi.js
@@ -1,6 +1,6 @@
 import axios from "axios";
-export const BASE_URL ="http://localhost:3001";
-// export const BASE_URL ="https://api.sstappstore.in";
+// export const BASE_URL ="http://localhost:3001";
+export const BASE_URL ="https://api.sstappstore.in";
 //getPackageWithProducts
 export const fetchPackageWithProducts = async (packageId) => {
   try {
